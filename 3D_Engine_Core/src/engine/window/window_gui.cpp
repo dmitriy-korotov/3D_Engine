e1c70a9f563b973f11b7e4d2c8032628f993f98c
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-08-19 19:37:35 +0300

    Added code drawing triangle for testing shaders

--- 3D_Engine_Core/src/engine/window/window_gui.cpp
+++ 3D_Engine_Core/src/engine/window/window_gui.cpp
@@ -9,6 +9,41 @@
 
 
 
+GLfloat points[] = {
+		-0.5f, -0.5f, 0.f,
+		0.f, 0.5f, 0.f,
+		0.5f, -0.5f, 0.f
+};
+
+GLfloat colors[] = {
+		1.f, 0.f, 0.f,
+		0.f, 1.f, 0.f,
+		0.f, 0.f, 1.f
+};
+
+const char* vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_poistion;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main() {"
+"	color = vertex_color;"
+"	gl_Position = vec4(vertex_poistion, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 460\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main() {"
+"	frag_color = vec4(color, 1.0);"
+"}";
+
+GLuint shader_program_ = 0;
+GLuint VAO_ = 0;
+
+
+
 namespace engine
 {
 	window_gui::window_gui(const std::string_view& _title)
@@ -37,6 +72,56 @@ namespace engine
 		ImGui_ImplOpenGL3_Init();
 		ImGui_ImplGlfw_InitForOpenGL(m_window_ptr_, true);
 
+
+
+		GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vs, 1, &vertex_shader, nullptr);
+		glCompileShader(vs);
+
+		int success;
+		char infoLog[512];
+		glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(vs, 512, NULL, infoLog);
+			LOG_CRITICAL("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n{}", infoLog);
+		}
+
+		GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fs, 1, &fragment_shader, nullptr);
+		glCompileShader(fs);
+
+		shader_program_ = glCreateProgram();
+		glAttachShader(shader_program_, vs);
+		glAttachShader(shader_program_, fs);
+		glLinkProgram(shader_program_);
+
+		glDetachShader(shader_program_, vs);
+		glDetachShader(shader_program_, fs);
+		glDeleteShader(vs);
+		glDeleteShader(fs);
+
+		GLuint points_vbo = 0;
+		glGenBuffers(1, &points_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+		GLuint colors_vbo = 0;
+		glGenBuffers(1, &colors_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+		glGenVertexArrays(1, &VAO_);
+		glBindVertexArray(VAO_);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
 		return std::nullopt;
 	}
 
@@ -47,6 +132,11 @@ namespace engine
 		glClearColor(m_bg_color_[0], m_bg_color_[1], m_bg_color_[2], m_bg_color_[3]);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
+		glUseProgram(shader_program_);
+		glBindVertexArray(VAO_);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		ImGuiIO& io_ = ImGui::GetIO();
 		io_.DisplaySize.x = static_cast<float>(getWidth());
 		io_.DisplaySize.y = static_cast<float>(getHeight());
