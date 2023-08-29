5d78868bbd2a5efc7dc59ceab25504eb01f8e099
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-08-29 12:06:56 +0300

    Finished class index_buffer

--- 3D_Engine_Core/include/engine/render/open_gl/index_buffer.hpp
+++ 3D_Engine_Core/include/engine/render/open_gl/index_buffer.hpp
@@ -1,24 +1,28 @@
 #pragma once
 
-#include <engine/render/open_gl/vertex_buffer.hpp>
+#include <engine/render/open_gl/basic_open_gl_buffer.hpp>
 
 
 
 namespace engine::render
 {
-	class index_buffer
+	class index_buffer final: public basic_open_gl_buffer
 	{
 	public:
 
-		index_buffer(const void* _data, size_t _count, vertex_buffer::Usage _usage_type);
+		index_buffer(const void* _data, uint64_t _count, Usage _usage_type);
+		index_buffer(index_buffer&& _other) noexcept;
+		index_buffer& operator=(index_buffer&& _right) noexcept;
+		~index_buffer() override;
 
-		void bind() const noexcept;
-		void unbind() const noexcept;
+		void bind() const noexcept override;
+		void unbind() const noexcept override;
+
+		uint64_t getIndexesCount() const noexcept;
 
 	private:
 
-		unsigned int m_id_ = 0;
-		unsigned int m_indexes_count_ = 0;
+		uint64_t m_indexes_count = 0;
 
 	};
 }
--- 3D_Engine_Core/src/engine/render/open_gl/index_buffer.cpp
+++ 3D_Engine_Core/src/engine/render/open_gl/index_buffer.cpp
@@ -6,18 +6,53 @@
 
 namespace engine::render
 {
-	index_buffer::index_buffer(const void* _data, size_t _count, vertex_buffer::Usage _usage_type)
+	index_buffer::index_buffer(const void* _data, uint64_t _count, Usage _usage_type)
+			: m_indexes_count(_count)
 	{
-		glGenBuffers(1, &m_id_);
+		glGenBuffers(1, &m_id);
 		bind();
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * _count, _data, GL_STATIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * _count, _data, usageToGLenum(_usage_type));
+	}
+
+
+
+	index_buffer::index_buffer(index_buffer&& _other) noexcept
+			: basic_open_gl_buffer(_other.m_id)
+			, m_indexes_count(_other.m_id)
+	{
+		_other.m_id = 0;
+		_other.m_indexes_count = 0;
+	}
+
+
+
+	index_buffer& index_buffer::operator=(index_buffer&& _right) noexcept
+	{
+		if (m_id != _right.m_id)
+		{
+			glDeleteBuffers(1, &m_id);
+
+			m_id = _right.m_id;
+			m_indexes_count = _right.m_indexes_count;
+
+			_right.m_id = 0;
+			_right.m_indexes_count = 0;
+		}
+		return *this;
+	}
+
+
+
+	index_buffer::~index_buffer()
+	{
+		glDeleteBuffers(1, &m_id);
 	}
 
 
 
 	void index_buffer::bind() const noexcept
 	{
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_id_);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_id);
 	}
 
 
@@ -25,5 +60,12 @@ namespace engine::render
 	void index_buffer::unbind() const noexcept
 	{
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+
+
+
+	uint64_t index_buffer::getIndexesCount() const noexcept
+	{
+		return m_indexes_count;
 	}
 }
