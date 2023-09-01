fe1165fbb9d3679cdd24c6bf617b36334d3d78e5
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-09-01 11:48:47 +0300

    Testing camera with imgui interface

--- 3D_Engine_Core/src/engine/window/window_gui.cpp
+++ 3D_Engine_Core/src/engine/window/window_gui.cpp
@@ -6,6 +6,8 @@
 #include <engine/render/open_gl/vertex_array.hpp>
 #include <engine/render/open_gl/buffer_layout.hpp>
 
+#include <engine/render/camera.hpp>
+
 #include <imgui/imgui.h>
 #include <imgui/backends/imgui_impl_opengl3.h>
 #include <imgui/backends/imgui_impl_glfw.h>
@@ -51,19 +53,26 @@ float scale[] = { 1.f, 1.f, 1.f };
 float translate[] = { 0.f, 0.f, 0.f };
 float rotate = 0.f;
 
+float camera_position[] = { 0.f, 0.f, 0.f };
+float camera_rotation[] = { 0.f, 0.f, 0.f };
+bool is_perspective_projection = false;
+
+
+
+std::unique_ptr<engine::render::camera> camera;
+
 
 
 const char* vertex_shader =
 		R"(#version 460
 		layout(location = 0) in vec3 vertex_poistion;
 		layout(location = 1) in vec3 vertex_color;
-		uniform mat4 scale_matrix;
-		uniform mat4 translate_matrix;
-		uniform mat4 rotate_matrix;
+		uniform mat4 model_matrix;
+		uniform mat4 view_projection_matrix;
 		out vec3 color;
 		void main() {
 			color = vertex_color;
-			gl_Position = translate_matrix * rotate_matrix * scale_matrix * vec4(vertex_poistion, 1.0);
+			gl_Position = view_projection_matrix * model_matrix * vec4(vertex_poistion, 1.0);
 		})";
 
 const char* fragment_shader =
@@ -159,6 +168,12 @@ namespace engine
 		VAO_1buffer_->addVertexBuffer(*points_colors_vbo_);
 		VAO_1buffer_->setIndexBuffer(*index_buffer_);
 
+
+
+		camera = std::make_unique<render::camera>();
+
+
+
 		return std::nullopt;
 	}
 
@@ -200,9 +215,17 @@ namespace engine
 								0, 0, 1, 0,
 								0, 0, 0, 1);
 
-		shader_program_->setMatrix4f("scale_matrix", scale_matrix);
-		shader_program_->setMatrix4f("translate_matrix", translate_matrix);
-		shader_program_->setMatrix4f("rotate_matrix", rotate_matrix);
+		glm::mat4 model_matrix = translate_matrix * rotate_matrix * scale_matrix;
+
+		shader_program_->setMatrix4f("model_matrix", model_matrix);
+
+		camera->setPositionAndRotation(glm::vec3(camera_position[0], camera_position[1], camera_position[2]),
+									   glm::vec3(camera_rotation[0], camera_rotation[1], camera_rotation[2]));
+		camera->setProjectionMode(is_perspective_projection ? render::camera::Projection::Perspective : render::camera::Projection::Orthographic);
+
+		shader_program_->setMatrix4f("view_projection_matrix", camera->getViewProjectionMatrix());
+
+
 
 		ImGuiIO& io_ = ImGui::GetIO();
 		io_.DisplaySize.x = static_cast<float>(getWidth());
@@ -218,6 +241,9 @@ namespace engine
 		ImGui::SliderFloat3("Scale", scale, 0.f, 2.f);
 		ImGui::SliderFloat3("Translate", translate, -1.f, 1.f);
 		ImGui::SliderFloat("Rotate", &rotate, 0.f, 360.f);
+		ImGui::SliderFloat3("Camera position", camera_position, -10.f, 10.f);
+		ImGui::SliderFloat3("Camera rotation", camera_rotation, 0.f, 360.f);
+		ImGui::Checkbox("Perspective projection", &is_perspective_projection);
 		ImGui::End();
 
 		ImGui::Render();
