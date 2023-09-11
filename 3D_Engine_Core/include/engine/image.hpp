37d9a02eac1f395e7c25285466b197e7cbf8c9f1
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-09-11 15:40:23 +0300

    Added image class

---
+++ 3D_Engine_Core/include/engine/image.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <engine/util/nocopyeble.hpp>
+
+#include <filesystem>
+
+
+
+namespace engine
+{
+	class image : util::nocopyeble
+	{
+	public:
+
+		image(const std::filesystem::path& _path_to_image);
+
+		int getWidth() const noexcept;
+		int getHeight() const noexcept;
+		int getChannels() const noexcept;
+		const uint8_t* getData() const noexcept;
+
+		bool isLoaded() const noexcept;
+
+	private:
+
+		bool m_is_loaded = false;
+
+		int m_width = 0;
+		int m_height = 0;
+		int m_channels = 0;
+
+		uint8_t* m_data = nullptr;
+
+	};
+}
---
+++ 3D_Engine_Core/src/engine/image.cpp
@@ -0,0 +1,52 @@
+#include <engine/image.hpp>
+
+#include <stb/image.h>
+
+
+
+namespace engine
+{
+	image::image(const std::filesystem::path& _path_to_image)
+	{
+		m_data = stbi_load(_path_to_image.generic_string().c_str(), &m_width, &m_height, &m_channels, 0);
+		if (m_data != nullptr)
+		{
+			m_is_loaded = true;
+		}
+	}
+
+
+
+	int image::getWidth() const noexcept
+	{
+		return m_width;
+	}
+
+
+
+	int image::getHeight() const noexcept
+	{
+		return m_height;
+	}
+
+
+
+	int image::getChannels() const noexcept
+	{
+		return m_channels;
+	}
+
+
+
+	const uint8_t* image::getData() const noexcept
+	{
+		return m_data;
+	}
+
+
+
+	bool image::isLoaded() const noexcept
+	{
+		return m_is_loaded;
+	}
+}
