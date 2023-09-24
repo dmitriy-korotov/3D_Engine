223a470327143c3050b1c9fe684f167723882711
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-09-24 13:32:16 +0300

    Added window common types file

---
+++ 3D_Engine_Core/include/engine/window/common_types.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdint.h>
+
+
+
+namespace engine::window
+{
+	using window_id_t = size_t;
+	constexpr window_id_t INVALID_WINDOW_ID = 0;
+
+	enum class OpenMode : uint8_t
+	{
+		FullScreen,
+		InWindow
+	};
+}
