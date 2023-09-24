8a2b599fed7eb47faf34f34a85e227d77310548e
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-09-24 15:04:49 +0300

    Added window impl enum

--- 3D_Engine_Core/include/engine/window/common_types.hpp
+++ 3D_Engine_Core/include/engine/window/common_types.hpp
@@ -9,9 +9,20 @@ namespace engine::window
 	using window_id_t = size_t;
 	constexpr window_id_t INVALID_WINDOW_ID = 0;
 
+
+
 	enum class OpenMode : uint8_t
 	{
 		FullScreen,
 		InWindow
+	};
+
+
+
+	enum class WindowImpl : uint8_t
+	{
+		GLFW,
+		SDL,
+		SFML
 	};
 }
