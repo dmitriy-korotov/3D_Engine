4e3e05c849105884a63bd92cbb235f45e075bf76
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-09-26 11:47:09 +0300

    Added mamterial class

---
+++ 3D_Engine_Core/include/engine/render/materials/open_gl/material.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <engine/render/materials/basic_material.hpp>
+#include <engine/render/open_gl/texture2D.hpp>
+
+#include <vector>
+
+
+
+namespace engine::render::materials::open_gl
+{
+	using render::open_gl::texture2D;
+
+	class material final : public basic_material 
+	{
+	public:
+
+		using texture_storage = std::vector<texture2D>;
+
+		material(texture_storage _textures) noexcept;
+
+		void use() const noexcept override;
+		void unuse() const noexcept override;
+
+	private:
+
+		texture_storage m_textures;
+
+	};
+}
---
+++ 3D_Engine_Core/src/engine/render/materials/open_gl/material.cpp
@@ -0,0 +1,26 @@
+#include <engine/render/materials/open_gl/material.hpp>
+
+
+
+namespace engine::render::materials::open_gl
+{
+	material::material(texture_storage _textures) noexcept
+			: m_textures(std::move(_textures))
+	{ }
+
+
+
+	void material::use() const noexcept
+	{
+		size_t unit = 0;
+		for (const auto& texture : m_textures)
+		{
+			texture.bind(unit++);
+		}
+	}
+
+
+
+	void material::unuse() const noexcept
+	{ }
+}
