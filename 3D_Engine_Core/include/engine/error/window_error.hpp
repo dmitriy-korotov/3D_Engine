db943d0e04496be833aa10997381cd686e25799d
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-09-30 11:57:16 +0300

    Added some errors in window_errors

--- 3D_Engine_Core/include/engine/error/window_error.hpp
+++ 3D_Engine_Core/include/engine/error/window_error.hpp
@@ -7,7 +7,9 @@ namespace engine::error
 	enum class window_error
 	{
 		can_not_create,
-		can_not_init_glfw,
+		can_not_init_GLFW,
+		can_not_init_SDL,
+		can_not_init_SFML,
 		can_not_add_new_window,
 		window_already_exists
 	};
