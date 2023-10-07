1bf0fb7899da650e313ccd221426592cb6a855c7
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-10-07 20:19:49 +0300

    Added basic_models_loader class

---
+++ 3D_Engine_Core/include/engine/render/util/basic_models_loader.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <engine/util/nocopyeble.hpp>
+
+#include <vector>
+#include <memory>
+#include <unordered_map>
+#include <filesystem>
+
+
+
+namespace engine::render::meshes
+{
+	class basic_mesh;
+}
+
+namespace engine::render::materials
+{
+	class basic_material;
+}
+
+namespace engine::render
+{
+	class basic_texure2D;
+}
+
+namespace engine::render::util
+{
+	using std::filesystem::path;
+
+	class basic_models_loader: private engine::util::nocopyeble
+	{
+	public:
+
+		using mesh_ptr = std::shared_ptr<render::meshes::basic_mesh>;
+		using material_ptr = std::shared_ptr<render::materials::basic_material>;
+		using mesh_storage = std::vector<mesh_ptr>;
+
+
+
+		basic_models_loader() = default;
+		virtual ~basic_models_loader() = default;
+
+		virtual void load(const path& _path_to_model) noexcept = 0;
+
+		mesh_storage& getMeshes() noexcept;
+		material_ptr& getMaterial() noexcept;
+
+		bool isLoaded() const noexcept;
+
+	protected:
+
+		bool m_is_loaded = false;
+
+		path m_model_directory;
+
+		mesh_storage m_meshes;
+		material_ptr m_material;
+
+	};
+}
---
+++ 3D_Engine_Core/src/engine/render/util/basic_models_loader.cpp
@@ -0,0 +1,25 @@
+#include <engine/render/util/basic_models_loader.hpp>
+
+
+
+namespace engine::render::util
+{
+	basic_models_loader::mesh_storage& basic_models_loader::getMeshes() noexcept
+	{
+		return m_meshes;
+	}
+
+
+
+	basic_models_loader::material_ptr& basic_models_loader::getMaterial() noexcept
+	{
+		return m_material;
+	}
+
+
+
+	bool basic_models_loader::isLoaded() const noexcept
+	{
+		return m_is_loaded;
+	}
+}
