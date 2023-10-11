7948859e777ca72d84c0aca42da53e1c58cea3e3
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-10-11 20:27:38 +0300

    Addded Engine class

---
+++ 3D_Engine_Core/include/engine/Engine.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <engine/util/noconstructible.hpp>
+
+#include <engine/render/application_renderer.hpp>
+#include <engine/modules/application_UIModule.hpp>
+
+
+
+namespace engine
+{
+	class Engine: private util::noconstructible
+	{
+	public:
+
+		using render_ptr = render::application_renderer::renderer_ptr;
+		using UIModule_ptr = modules::application_UIModule::UIModule_ptr;
+
+		static void setApplicationRenderer(render_ptr _renderer) noexcept;
+		static void setApplicationUIModule(UIModule_ptr _UI_module) noexcept;
+
+		static const render_ptr& getApplicationRenderer() noexcept;
+		static const UIModule_ptr& getApplicationUIModule() noexcept;
+
+	};
+}
---
+++ 3D_Engine_Core/src/engine/Engine.cpp
@@ -0,0 +1,32 @@
+#include <engine/Engine.hpp>
+
+
+
+namespace engine
+{
+	void Engine::setApplicationRenderer(render_ptr _renderer) noexcept
+	{
+		render::application_renderer::instance().setupRenderer(std::move(_renderer));
+	}
+
+
+
+	void Engine::setApplicationUIModule(UIModule_ptr _UI_module) noexcept
+	{
+		modules::application_UIModule::instance().setUIModule(std::move(_UI_module));
+	}
+
+
+
+	const Engine::render_ptr& Engine::getApplicationRenderer() noexcept
+	{
+		return render::application_renderer::instance().getRenderer();
+	}
+
+
+
+	const Engine::UIModule_ptr& Engine::getApplicationUIModule() noexcept
+	{
+		modules::application_UIModule::instance().getUIModule();
+	}
+}
