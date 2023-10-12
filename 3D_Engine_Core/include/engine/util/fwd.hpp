730fef2fe723faa787c14b6b456a3c31a8e4257f
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-10-12 22:44:36 +0300

    Added fwd files for util

---
+++ 3D_Engine_Core/include/engine/util/fwd.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <engine/util/fwd/file_reader.hpp>
+#include <engine/util/fwd/noconstructible.hpp>
+#include <engine/util/fwd/nocopyeble.hpp>
+#include <engine/util/fwd/nomoveble.hpp>
---
+++ 3D_Engine_Core/include/engine/util/fwd/file_reader.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace engine::util
+{
+	class file_reader;
+}
