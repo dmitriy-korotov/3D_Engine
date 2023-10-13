df3a4526308caa294b2db03810cda944ed04a922
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-10-13 11:52:46 +0300

    Added fwd in input

---
+++ 3D_Engine_Core/include/engine/input/fwd.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <engine/input/fwd/keyboard.hpp>
+#include <engine/input/fwd/keys.hpp>
+#include <engine/input/fwd/mouse.hpp>
---
+++ 3D_Engine_Core/include/engine/input/fwd/keys.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+
+
+
+namespace engine::input
+{
+	enum class Action : uint8_t;
+	enum class Key : uint16_t;
+	enum class MouseButton : uint8_t;
+}
