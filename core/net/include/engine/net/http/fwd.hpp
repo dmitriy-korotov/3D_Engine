c7cac43dbf28f6fd50032894c5ffd580a418aa89
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-12-18 15:17:45 +0300

    Added fwd files in net library

---
+++ core/net/include/engine/net/http/fwd.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <engine/net/http/fwd/basic_http_message.hpp>
+#include <engine/net/http/fwd/basic_http_message_parser.hpp>
+#include <engine/net/http/fwd/http_client.hpp>
+#include <engine/net/http/fwd/http_server.hpp>
+#include <engine/net/http/fwd/json_body.hpp>
+#include <engine/net/http/fwd/request.hpp>
+#include <engine/net/http/fwd/request_parser.hpp>
+#include <engine/net/http/fwd/response.hpp>
+#include <engine/net/http/fwd/response_parser.hpp>
+#include <engine/net/http/fwd/session.hpp>
+#include <engine/net/http/fwd/string_body.hpp>
